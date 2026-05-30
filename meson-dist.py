67839054e32fbb117de0ee07533981ac70e68f55
fujiwarat <takao.fujiwara1@gmail.com>
2026-05-31 08:39:47 +0900

    Release 1.5.35-alpha1
    Closes: #2913

--- meson-dist.py
+++ meson-dist.py
@@ -34,6 +34,40 @@ BUILD_ROOT = os.getenv('MESON_PROJECT_BUILD_ROOT', '.')
 SOURCE_ROOT = os.getenv('MESON_PROJECT_SOURCE_ROOT', '.')
 DIST_ROOT = os.getenv('MESON_PROJECT_DIST_ROOT', '.')
 
+keymap_exception = [ 'common', 'meson.build', 'modifiers',
+                     'us', 'jp', 'kr', 'in'
+                   ]
+ignore_file_pairs = [
+    { 'name': 'common',
+      'paths': [ '.github', '.gitignore', '.travis.yml',
+                 'codereview.settings',
+               ],
+    },
+    { 'name': 'ibus',
+      'paths': [ 'conf/gconf',
+                 'data/ibus.schemas.in', 'data/icons/ibus-help.png',
+                 'data/icons/ibus-locale.svg', 'data/icons/ibus-zh.svg',
+                 'data/keymaps', 'ibus', 'src/python',
+                 'src/tests/ibus-engine.c',
+                 'src/tests/ibus-global-engine.c',
+                 'src/tests/ibus-keymap.c',
+                 'src/tests/ibus-proxy.c',
+                 'ui/gtk2',
+               ],
+    },
+]
+
+
+def run_git_subprocess_withreturncode(command):
+    try:
+        result = subprocess.run(command, shell = True)
+        if result.returncode != 0:
+            sys.exit(result.returncode)
+    except subprocess.CalledProcessError as e:
+        print(f'Failed to run git command {command}: {e}', file=sys.stderr)
+        sys.exit(1)
+
+
 cmd = ['meson', 'introspect', '--projectinfo', BUILD_ROOT]
 info = None
 try:
@@ -64,43 +98,71 @@ if name_str == None or version_str == None:
 versions = version_str.split('.')
 major_version = int(versions[0])
 minor_version = int(versions[1])
-micro_version = int(versions[2])
+micro_version = int(versions[2].split('-')[0])
 if IBUS_PREV_VERSION == None:
     prev_micro_version = micro_version - 1
     IBUS_PREV_VERSION = '%d.%d.%d' % (major_version,
                                       minor_version,
                                       prev_micro_version)
-subprocess.run('git log --name-status --date=iso > "{0}/{1}"'.format(
-               DIST_ROOT, 'ChangeLog'),
-               shell = True)
-subprocess.run('echo "Changes in {0} {1}" > "{2}/{3}"'.format(
-               name_str, version_str,
-               DIST_ROOT, 'NEWS'),
-               shell = True)
-subprocess.run('echo "" >> "{0}/{1}"'.format(
-               DIST_ROOT, 'NEWS'),
-               shell = True)
-subprocess.run('git shortlog {0}...{1} >> "{2}/{3}"'.format(
-               IBUS_PREV_VERSION, version_str,
-               DIST_ROOT, 'NEWS'),
-               shell = True)
-subprocess.run('echo "" >> "{0}/{1}"'.format(
-               DIST_ROOT, 'NEWS'),
-               shell = True)
-subprocess.run('git log {0}...{1} {2} >> "{3}/{4}"'.format(
-               IBUS_PREV_VERSION, version_str,
-               '--reverse --pretty=format:"%s (%an) %h"',
-               DIST_ROOT, 'NEWS'),
-               shell = True)
 
-shutil.copy2('autogen.sh', f'{DIST_ROOT}')
-# Do you wish to copy configure and Makefile.in ?
+run_git_subprocess_withreturncode(
+    'git log --name-status --date=iso > "{0}/{1}"'.format(
+    DIST_ROOT, 'ChangeLog'))
+run_git_subprocess_withreturncode(
+    'echo "Changes in {0} {1}" > "{2}/{3}"'.format(
+    name_str, version_str,
+    DIST_ROOT, 'NEWS'))
+run_git_subprocess_withreturncode(
+    'echo "" >> "{0}/{1}"'.format(
+    DIST_ROOT, 'NEWS'))
+run_git_subprocess_withreturncode(
+    'git shortlog {0}...{1} >> "{2}/{3}"'.format(
+    IBUS_PREV_VERSION, version_str,
+    DIST_ROOT, 'NEWS'))
+run_git_subprocess_withreturncode(
+    'echo "" >> "{0}/{1}"'.format(
+    DIST_ROOT, 'NEWS'))
+run_git_subprocess_withreturncode(
+    'git log {0}...{1} {2} >> "{3}/{4}"'.format(
+    IBUS_PREV_VERSION, version_str,
+    '--reverse --pretty=format:"%s (%an) %h"',
+    DIST_ROOT, 'NEWS'))
+
+try:
+    shutil.copy2('autogen.sh', f'{DIST_ROOT}')
+    # Do you wish to copy configure and Makefile.in ?
+except FileNotFoundError as e:
+    print(f'Failed to copy autogen.sh: {e}', file=sys.stderr)
+    sys.exit(1)
 
 os.chdir(DIST_ROOT)
-for subdir in [ 'ibus', 'ui/gtk2' ]:
-    deprecated_path = Path(subdir)
-    if deprecated_path.exists() and deprecated_path.is_dir():
-        shutil.rmtree(deprecated_path)
-        print(f'Delete deprecated {str(deprecated_path)}.')
-    else:
-        print(f'{str(deprecated_path)} does not exist or is not a directory.')
+# Should ignore deprecated files or Git files.
+for ignore_file_pair in ignore_file_pairs:
+    _name = ignore_file_pair['name']
+    if _name != 'common' and  _name != name_str:
+        continue
+    for _path in ignore_file_pair['paths']:
+        ignored_path = Path(_path)
+        if ignored_path.exists():
+            try:
+                if _name == name_str and \
+                   ignored_path.samefile(Path('data/keymaps')):
+                    if not ignored_path.is_dir():
+                        print(f'{ignored_path} should be a directory',
+                              file=sys.stderr)
+                        sys.exit(1)
+                    for child in ignored_path.iterdir():
+                        if not child.name in keymap_exception:
+                            child.unlink()
+                    print(f'Delete deprecated files in {ignored_path}')
+                else:
+                    if ignored_path.is_dir():
+                        shutil.rmtree(ignored_path)
+                    else:
+                        ignored_path.unlink()
+                    print(f'Delete deprecated {ignored_path}')
+            except Exception as e:
+                print(f'Failed to rm {ignored_path}: {e}', file=sys.stderr)
+                sys.exit(1)
+        elif _name == name_str:
+            print(f'{ignored_path} does not exist')
